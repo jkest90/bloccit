6ecb9c878a5cbd6d539fd973c16b2138e365676f
jkest90 <jkest618@gmail.com>
2018-12-06 20:57:43 -0700

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+
+
+const appConfig = require('./config/main-config.js');
 const routeConfig = require('./config/route-config.js');
 
 // initialize routes
 routeConfig.init(app);
+// initialize env variables stored in .env
+appConfig.init();
 
 module.exports = app;
