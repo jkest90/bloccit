b1e8543b9d2c719e300c8c14af61b5d6c39a581d
jkest90 <jkest618@gmail.com>
2019-01-15 14:48:06 -0700

    mentor help: validations cp

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const path = require('path');
 const viewsFolder = path.join(__dirname, '..', 'views');
 const bodyParser = require('body-parser');
+const expressValidator = require("express-validator");
+const session = require("express-session");
+const flash = require("express-flash");
 
 module.exports = {
    // function to initialize our Express app:
@@ -12,6 +15,17 @@ module.exports = {
       app.set('view engine', 'ejs');
       // configure body-parser for urlencoded bodies with extended: true - to send rich objects.
       app.use(bodyParser.urlencoded({ extended: true }));
+      // mount express-validator middlewear to validate incoming data.
+      app.use(expressValidator());
+      // configure and mount express session middleweaer
+      app.use(session({
+         secret: process.env.cookieSecret,
+         resave: false,
+         saveUninitialized: false,
+         cookie: { maxAge: 60000 }
+      }));
+      // mount flash middlwear
+      app.use(flash());
       // tell express where to find static assets.
       app.use(express.static(path.join(__dirname, '..', 'assets')));
    }
