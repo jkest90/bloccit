b5f2916e6d6d8820d52f46127893fd3c89702e57
jkest90 <jkest618@gmail.com>
2019-01-15 16:47:23 -0700

    validation assignment: complete

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -122,15 +122,15 @@ describe("routes : topics", () => {
    });
 
    describe("POST /topics/create", () => {
-      const options = {
-         url: `${base}create`,
-         form: {
-            title: "blink-182 songs",
-            description: "What's your favorite blink-182 song?"
-         }
-      };
 
       it("should create a new topic and redirect", (done) => {
+         const options = {
+            url: `${base}create`,
+            form: {
+               title: "blink-182 songs",
+               description: "What's your favorite blink-182 song?"
+            }
+         };
 
          request.post(options,
             // post method takes two arguments.
@@ -147,10 +147,35 @@ describe("routes : topics", () => {
                .catch((err) => {
                   console.log(err);
                   done();
+               }
+            );
+         });
+      });
+
+      it("should not create a topic that fails validation", (done) => {
+         const options = {
+            url: `${base}create`,
+            form: {
+               title: "C",
+               description: "D"
+            }
+         };
+
+         request.post(options,
+            (err, res, body) => {
+               Topic.findOne({ where: {title: "C" }})
+               .then((topic) => {
+                  expect(topic).toBeNull();
+                  done();
+               })
+               .catch((err) => {
+                  console.log(err);
+                  done();
                });
-            })
-         }
-      );
+            }
+         );
+      });
+
    });
 
 });
